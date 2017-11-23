64623ead85fe678952b0539e0c68cb001b9a9e1c
zhangcheng <cheng.zhang@bolemart.com>
2017-11-23 18:48:23 +0800

    CollapsingToolbarLayout的使用

---
+++ tablayoutdemo/src/main/java/com/example/tablayoutdemo/FabBehavior.java
@@ -0,0 +1,68 @@
+package com.example.tablayoutdemo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by cc on 2017/11/22.
+ */
+
+public class FabBehavior extends FloatingActionButton.Behavior {
+
+    private boolean visible = true;
+
+    public FabBehavior() {
+    }
+
+    public FabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        //观察View开始滑动的时候回调
+        Log.d("123", "开始滑动");
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild,
+                target, axes, type);
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+        //正在滑动
+        Log.d("123", "onNestedScroll: " + "dyConsumed:" + dyConsumed + ",dyUnconsumed:" + dyUnconsumed);
+
+        if (dyConsumed > 0 && visible) {
+            //隐藏
+            visible = false;
+            onHide(child);
+        } else if (dyConsumed < 0 && !visible) {
+            //显示
+            visible = true;
+            onShow(child);
+        }
+    }
+
+    private void onShow(FloatingActionButton child) {
+        //平移动画
+        // ViewCompat.animate(child).translationY(0).setInterpolator(new DecelerateInterpolator(3)).start();
+        //缩放动画
+        ViewCompat.animate(child).scaleY(1.0f).scaleX(1.0f).start();
+    }
+
+    private void onHide(FloatingActionButton child) {
+
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+        //平移动画
+        //ViewCompat.animate(child).translationY(child.getHeight() + layoutParams.bottomMargin).setInterpolator(new AccelerateInterpolator(3)).start();
+        //缩放动画
+        ViewCompat.animate(child).scaleY(0).scaleX(0).start();
+    }
+}
